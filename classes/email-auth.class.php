404d55d8feff4e06ee9c81655405e8295d9dfdae
Woga65 <woga@mytsa.de>
2024-01-18 07:19:58 +0100

    Fixed changes to email-auth.class.php not committed

--- classes/email-auth.class.php
+++ classes/email-auth.class.php
@@ -4,11 +4,13 @@ class EmailAuth {
     private function __construct() {}
 
     public static function sendVerification($email, $name, $vkey) {
+        $config = App::resolve("config")["verification"];
+
         $recipient = $email;
-        $subject = "your registration: Timekeeping Project";
-        $headers = "From: Timekeeping <noreply@ws-timekeeping.de>";
+        $subject = $config["subject"];
+        $headers = $config["from"];
         $message = "Hi $name,\r\n \r\nto complete the registration process, please follow the link below: \r\n \r\n";
-        $message = $message . "https://wolfgang-siebert.de/projects/login-component/verify.php?vkey=$vkey";                           
+        $message = $message . $config["scriptUrl"] . "?vkey=$vkey";                           
         mail($recipient, $subject, $message, $headers);
         return true;
     }
